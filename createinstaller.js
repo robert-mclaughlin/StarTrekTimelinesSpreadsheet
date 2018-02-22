9f65c25e28473de17676e3855a3fa8675bfaa8cb
IAmPicard <tekmanro@yahoo.com>
2018-02-21 17:57:58 -0800

    report installer error

--- createinstaller.js
+++ createinstaller.js
@@ -8,14 +8,17 @@ const outPath = path.join(rootPath, 'builds');
 // Promise is returned
 builder.build({
 	targets: Platform.WINDOWS.createTarget(),
-	prepackaged: path.join(outPath, 'startrektimelinestool-win32-x64'),
+	prepackaged: path.join(outPath, 'Star Trek Timelines Crew Management-win32-x64'),
 	config: {
-		win: { target: ['nsis', '7z'] }
+		win: {
+			target: ['nsis', '7z'],
+			icon: path.join(rootPath, 'src/assets/icons/ATFleet.ico')
+		}
 	}
 })
 	.then(() => {
 		// handle result
 	})
 	.catch((error) => {
-		// handle error
+		console.error(error);
 	});
