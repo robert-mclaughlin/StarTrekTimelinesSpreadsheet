d861f1c59d8588d93f7d89bd3c7d3f258725818a
IAmPicard <crewmanifest@gmail.com>
2018-06-27 19:12:27 -0700

    Add a way to upload gauntlet rounds to Google Sheets (untested)

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -7,6 +7,7 @@ import { CONFIG } from 'sttapi';
 
 export class LoggerClass {
     basePath;
+    exportFields;
 
     constructor() {
         const app = electron.app || electron.remote.app;
@@ -15,11 +16,209 @@ export class LoggerClass {
         if (!fs.existsSync(this.basePath)) {
             fs.mkdirSync(this.basePath);
         }
+
+        this.exportFields = [
+            {
+                label: 'Featured skill',
+                value: (row) => CONFIG.SKILLS[row.data.gauntlet.contest_data.featured_skill]
+            },
+            {
+                label: 'Featured traits',
+                value: (row) => row.data.gauntlet.contest_data.traits.map(trait => STTApi.getTraitName(trait)).join(', ')
+            },
+            {
+                label: 'bracket_id',
+                value: (row) => row.data.gauntlet.bracket_id
+            },
+            {
+                label: 'Consecutive Wins',
+                value: (row) => row.consecutive_wins
+            },
+            {
+                label: 'Player 1',
+                value: (row) => row.data.lastResult.player_rolls[0]
+            },
+            {
+                label: 'PCrit 1',
+                value: (row) => row.data.lastResult.player_crit_rolls[0]
+            },
+            {
+                label: 'Player 2',
+                value: (row) => row.data.lastResult.player_rolls[1]
+            },
+            {
+                label: 'PCrit 2',
+                value: (row) => row.data.lastResult.player_crit_rolls[1]
+            },
+            {
+                label: 'Player 3',
+                value: (row) => row.data.lastResult.player_rolls[2]
+            },
+            {
+                label: 'PCrit 3',
+                value: (row) => row.data.lastResult.player_crit_rolls[2]
+            },
+            {
+                label: 'Player 4',
+                value: (row) => row.data.lastResult.player_rolls[3]
+            },
+            {
+                label: 'PCrit 4',
+                value: (row) => row.data.lastResult.player_crit_rolls[3]
+            },
+            {
+                label: 'Player 5',
+                value: (row) => row.data.lastResult.player_rolls[4]
+            },
+            {
+                label: 'PCrit 5',
+                value: (row) => row.data.lastResult.player_crit_rolls[4]
+            },
+            {
+                label: 'Player 6',
+                value: (row) => row.data.lastResult.player_rolls[5]
+            },
+            {
+                label: 'PCrit 6',
+                value: (row) => row.data.lastResult.player_crit_rolls[5]
+            },
+            {
+                label: 'Opponent 1',
+                value: (row) => row.data.lastResult.opponent_rolls[0]
+            },
+            {
+                label: 'OCrit 1',
+                value: (row) => row.data.lastResult.opponent_crit_rolls[0]
+            },
+            {
+                label: 'Opponent 2',
+                value: (row) => row.data.lastResult.opponent_rolls[1]
+            },
+            {
+                label: 'OCrit 2',
+                value: (row) => row.data.lastResult.opponent_crit_rolls[1]
+            },
+            {
+                label: 'Opponent 3',
+                value: (row) => row.data.lastResult.opponent_rolls[2]
+            },
+            {
+                label: 'OCrit 3',
+                value: (row) => row.data.lastResult.opponent_crit_rolls[2]
+            },
+            {
+                label: 'Opponent 4',
+                value: (row) => row.data.lastResult.opponent_rolls[3]
+            },
+            {
+                label: 'OCrit 4',
+                value: (row) => row.data.lastResult.opponent_crit_rolls[3]
+            },
+            {
+                label: 'Opponent 5',
+                value: (row) => row.data.lastResult.opponent_rolls[4]
+            },
+            {
+                label: 'OCrit 5',
+                value: (row) => row.data.lastResult.opponent_crit_rolls[4]
+            },
+            {
+                label: 'Opponent 6',
+                value: (row) => row.data.lastResult.opponent_rolls[5]
+            },
+            {
+                label: 'OCrit 6',
+                value: (row) => row.data.lastResult.opponent_crit_rolls[5]
+            },
+            {
+                label: 'Won round',
+                value: (row) => row.data.lastResult.win ? 'Yes' : 'No'
+            },
+            {
+                label: 'loot_box_rarity',
+                value: (row) => row.data.lastResult.loot_box_rarity
+            },
+            {
+                label: 'Skill 1',
+                value: (row) => CONFIG.SKILLS[row.data.gauntlet.contest_data.primary_skill]
+            },
+            {
+                label: 'Skill 2',
+                value: (row) => CONFIG.SKILLS[row.data.gauntlet.contest_data.secondary_skill]
+            },
+            {
+                label: 'Player crew',
+                value: (row) => STTApi.getCrewAvatarBySymbol(row.match.crewOdd.archetype_symbol).name
+            },
+            {
+                label: 'Player crit chance',
+                value: (row) => row.match.crewOdd.crit_chance
+            },
+            {
+                label: 'PSkill1 min',
+                value: (row) => row.match.crewOdd.min[0]
+            },
+            {
+                label: 'PSkill1 max',
+                value: (row) => row.match.crewOdd.max[0]
+            },
+            {
+                label: 'PSkill2 min',
+                value: (row) => row.match.crewOdd.min[1]
+            },
+            {
+                label: 'PSkill2 max',
+                value: (row) => row.match.crewOdd.max[1]
+            },
+            {
+                label: 'Crew uses',
+                value: (row) => row.match.crewOdd.used
+            },
+            {
+                label: 'Opponent crew',
+                value: (row) => STTApi.getCrewAvatarBySymbol(row.match.opponent.archetype_symbol).name
+            },
+            {
+                label: 'Opponent crit chance',
+                value: (row) => row.match.opponent.crit_chance
+            },
+            {
+                label: 'OSkill1 min',
+                value: (row) => row.match.opponent.min[0]
+            },
+            {
+                label: 'OSkill1 max',
+                value: (row) => row.match.opponent.max[0]
+            },
+            {
+                label: 'OSkill2 min',
+                value: (row) => row.match.opponent.min[1]
+            },
+            {
+                label: 'OSkill2 max',
+                value: (row) => row.match.opponent.max[1]
+            },
+            {
+                label: 'Estimated chance',
+                value: (row) => row.match.chance
+            },
+            {
+                label: 'Time (epoch)',
+                value: (row) => row.time
+            }
+        ];
     }
 
     logGauntletEntry(data, match, consecutive_wins) {
         if (data && data.gauntlet && data.gauntlet.gauntlet_id) {
             let fileName = `${this.basePath}gauntlet_log_${data.gauntlet.gauntlet_id}.json`;
+
+            let results = {
+                data: data,
+                match: match,
+                consecutive_wins: consecutive_wins,
+                time: Date.now()
+            };
 
             // TODO: This can be smarter, perhaps with an in-memory cache, to avoid reading the whole thing and rewriting it with every entry
             fs.exists(fileName, (exists) => {
@@ -27,24 +226,26 @@ export class LoggerClass {
                     fs.readFile(fileName, (err, inFile) => {
                         if (!err) {
                             let arr = JSON.parse(inFile);
-                            arr.push({
-                                data: data,
-                                match: match,
-                                consecutive_wins: consecutive_wins,
-                                time: Date.now()
-                            });
+                            arr.push(results);
                             fs.writeFile(fileName, JSON.stringify(arr), (err) => { if (err) console.error(err); });
                         }
                     });
                 } else {
-                    fs.writeFile(fileName, JSON.stringify([{
-                        data: data,
-                        match: match,
-                        consecutive_wins: consecutive_wins,
-                        time: Date.now()
-                    }]), (err) => { if (err) console.error(err); });
+                    fs.writeFile(fileName, JSON.stringify([results]), (err) => { if (err) console.error(err); });
                 }
             });
+
+            // Upload the result to a Google Sheet
+
+            //TODO: there should be an opt-out setting
+            const flowURL = 'https://prod-55.westus.logic.azure.com:443/workflows/dda0b81f61964c08ac5baf2dcd6ba1c8/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=04L6hPWcEIpAVI1oI8rSct0_XR4wDW4NMWjpDvW85V0';
+
+            let sheetRow = {};
+            this.exportFields.forEach(entry => {
+                sheetRow[entry.label] = entry.value();
+            });
+
+            STTApi.networkHelper().postjson(flowURL, [sheetRow]);
 
             return fileName;
         }
@@ -68,202 +269,8 @@ export class LoggerClass {
 
             fs.readFile(logPath, (err, inFile) => {
                 if (!err) {
-
-                    let sum = (a,c) => a + c;
-
-                    var fields = [
-                        {
-                            label: 'Featured skill',
-                            value: (row) => CONFIG.SKILLS[row.data.gauntlet.contest_data.featured_skill]
-                        },
-                        {
-                            label: 'Featured traits',
-                            value: (row) => row.data.gauntlet.contest_data.traits.map(trait => STTApi.getTraitName(trait)).join(', ')
-                        },
-                        {
-                            label: 'bracket_id',
-                            value: (row) => row.data.gauntlet.bracket_id
-                        },
-                        {
-                            label: 'Consecutive Wins',
-                            value: (row) => row.consecutive_wins
-                        },
-                        {
-                            label: 'Player 1',
-                            value: (row) => row.data.lastResult.player_rolls[0]
-                        },
-                        {
-                            label: 'PCrit 1',
-                            value: (row) => row.data.lastResult.player_crit_rolls[0]
-                        },
-                        {
-                            label: 'Player 2',
-                            value: (row) => row.data.lastResult.player_rolls[1]
-                        },
-                        {
-                            label: 'PCrit 2',
-                            value: (row) => row.data.lastResult.player_crit_rolls[1]
-                        },
-                        {
-                            label: 'Player 3',
-                            value: (row) => row.data.lastResult.player_rolls[2]
-                        },
-                        {
-                            label: 'PCrit 3',
-                            value: (row) => row.data.lastResult.player_crit_rolls[2]
-                        },
-                        {
-                            label: 'Player 4',
-                            value: (row) => row.data.lastResult.player_rolls[3]
-                        },
-                        {
-                            label: 'PCrit 4',
-                            value: (row) => row.data.lastResult.player_crit_rolls[3]
-                        },
-                        {
-                            label: 'Player 5',
-                            value: (row) => row.data.lastResult.player_rolls[4]
-                        },
-                        {
-                            label: 'PCrit 5',
-                            value: (row) => row.data.lastResult.player_crit_rolls[4]
-                        },
-                        {
-                            label: 'Player 6',
-                            value: (row) => row.data.lastResult.player_rolls[5]
-                        },
-                        {
-                            label: 'PCrit 6',
-                            value: (row) => row.data.lastResult.player_crit_rolls[5]
-                        },
-                        {
-                            label: 'Opponent 1',
-                            value: (row) => row.data.lastResult.opponent_rolls[0]
-                        },
-                        {
-                            label: 'OCrit 1',
-                            value: (row) => row.data.lastResult.opponent_crit_rolls[0]
-                        },
-                        {
-                            label: 'Opponent 2',
-                            value: (row) => row.data.lastResult.opponent_rolls[1]
-                        },
-                        {
-                            label: 'OCrit 2',
-                            value: (row) => row.data.lastResult.opponent_crit_rolls[1]
-                        },
-                        {
-                            label: 'Opponent 3',
-                            value: (row) => row.data.lastResult.opponent_rolls[2]
-                        },
-                        {
-                            label: 'OCrit 3',
-                            value: (row) => row.data.lastResult.opponent_crit_rolls[2]
-                        },
-                        {
-                            label: 'Opponent 4',
-                            value: (row) => row.data.lastResult.opponent_rolls[3]
-                        },
-                        {
-                            label: 'OCrit 4',
-                            value: (row) => row.data.lastResult.opponent_crit_rolls[3]
-                        },
-                        {
-                            label: 'Opponent 5',
-                            value: (row) => row.data.lastResult.opponent_rolls[4]
-                        },
-                        {
-                            label: 'OCrit 5',
-                            value: (row) => row.data.lastResult.opponent_crit_rolls[4]
-                        },
-                        {
-                            label: 'Opponent 6',
-                            value: (row) => row.data.lastResult.opponent_rolls[5]
-                        },
-                        {
-                            label: 'OCrit 6',
-                            value: (row) => row.data.lastResult.opponent_crit_rolls[5]
-                        },
-                        {
-                            label: 'Won round',
-                            value: (row) => row.data.lastResult.win ? 'Yes' : 'No'
-                        },
-                        {
-                            label: 'loot_box_rarity',
-                            value: (row) => row.data.lastResult.loot_box_rarity
-                        },
-                        {
-                            label: 'Skill 1',
-                            value: (row) => CONFIG.SKILLS[row.data.gauntlet.contest_data.primary_skill]
-                        },
-                        {
-                            label: 'Skill 2',
-                            value: (row) => CONFIG.SKILLS[row.data.gauntlet.contest_data.secondary_skill]
-                        },
-                        {
-                            label: 'Player crew',
-                            value: (row) => STTApi.getCrewAvatarBySymbol(row.match.crewOdd.archetype_symbol).name
-                        },
-                        {
-                            label: 'Player crit chance',
-                            value: 'match.crewOdd.crit_chance'
-                        },
-                        {
-                            label: 'PSkill1 min',
-                            value: (row) => row.match.crewOdd.min[0]
-                        },
-                        {
-                            label: 'PSkill1 max',
-                            value: (row) => row.match.crewOdd.max[0]
-                        },
-                        {
-                            label: 'PSkill2 min',
-                            value: (row) => row.match.crewOdd.min[1]
-                        },
-                        {
-                            label: 'PSkill2 max',
-                            value: (row) => row.match.crewOdd.max[1]
-                        },
-                        {
-                            label: 'Crew uses',
-                            value: 'match.crewOdd.used'
-                        },
-                        {
-                            label: 'Opponent crew',
-                            value: (row) => STTApi.getCrewAvatarBySymbol(row.match.opponent.archetype_symbol).name
-                        },
-                        {
-                            label: 'Opponent crit chance',
-                            value: 'match.opponent.crit_chance'
-                        },
-                        {
-                            label: 'OSkill1 min',
-                            value: (row) => row.match.opponent.min[0]
-                        },
-                        {
-                            label: 'OSkill1 max',
-                            value: (row) => row.match.opponent.max[0]
-                        },
-                        {
-                            label: 'OSkill2 min',
-                            value: (row) => row.match.opponent.min[1]
-                        },
-                        {
-                            label: 'OSkill2 max',
-                            value: (row) => row.match.opponent.max[1]
-                        },
-                        {
-                            label: 'Estimated chance',
-                            value: 'match.chance'
-                        },
-                        {
-                            label: 'Time (epoch)',
-                            value: 'time'
-                        }
-                    ];
-
                     // Format as CSV
-                    let csv = json2csv(JSON.parse(inFile), { fields });
+                    let csv = json2csv(JSON.parse(inFile), { fields: this.exportFields });
 
                     fs.writeFile(fileName, csv, function (err) {
                         if (err) { reject(err); }
