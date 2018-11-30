cf040ace09b96dd111f89f1797c5f1d8b3519da5
IAmPicard <crewmanifest@gmail.com>
2018-11-29 19:00:15 -0800

    Start working on shuttle calculator; doesn't do much yet.

--- src/components/Shuttles.js
+++ src/components/Shuttles.js
@@ -50,13 +50,156 @@ export class Shuttles extends React.Component {
 				tokens: event.content.shuttles.map(s => s.token),
 				nextVP: eventVP ? eventVP.quantity : 0
 			};
-		}
+        }
+        
+        this.slotCalculator();
 
 		this.state = {
 			currentEvent,
 			eventImageUrl: undefined,
 			shuttles: STTApi.playerData.character.shuttle_adventures.map(adventure => adventure.shuttles[0])
 		};
+	}
+
+	calcCrew(shuttle) {
+		const getCrewSkill = (crew, skill) => {
+			let val = crew[skill + '_core'];
+			if (crew.tired) {
+				val *= STTApi.serverConfig.config.conflict.tired_crew_coefficient;
+			}
+
+			return val;
+		};
+
+		const calcSkill = (slot, crew, boost) => {
+			let secondary_skill_percentage = STTApi.serverConfig.config.shuttle_adventures.secondary_skill_percentage;
+
+			let winnerComboVal = 0;
+			let totalFromBoost = 0;
+			slot.skills.forEach(skill => {
+				let andSkills = skill.split(',');
+
+				andSkills.forEach(ors => {
+					if (boost && boost.bonuses && boost.bonuses[ors]) {
+						totalFromBoost += boost.bonuses[ors].core;
+					}
+				});
+
+				andSkills = andSkills
+					.map(ors => getCrewSkill(crew, ors))
+					.sort()
+					.reverse();
+
+				let val = 0;
+				for (let k = 0; k < andSkills.Length; k++) {
+					if (k == 0) {
+						val += andSkills[k];
+					} else {
+						val += andSkills[k] * secondary_skill_percentage;
+					}
+				}
+
+				val += totalFromBoost;
+
+				winnerComboVal = Math.max(winnerComboVal, val);
+			});
+
+			// TODO: what is slot.trait_bonuses? Looks like an empty object; never used
+
+			let bonusMultiplier = 1; // TODO get bonus for crew from current event
+
+			return winnerComboVal * bonusMultiplier;
+		};
+
+		let skillSum = 0;
+		shuttle.slots.forEach(slot => {
+			skillSum += calcSkill(slot, crew, boost);
+		});
+
+		let challengeRating = shuttleAdventure.challenge_rating * shuttle.slots.length;
+		let sigmoid_steepness = STTApi.serverConfig.config.shuttle_adventures.sigmoid_steepness;
+		let sigmoid_midpoint = STTApi.serverConfig.config.shuttle_adventures.sigmoid_midpoint;
+		let percent = 1 / (1 + Math.exp(-sigmoid_steepness * (skillSum / challengeRating - sigmoid_midpoint)));
+
+		let actualPercent = percent * 100;
+	}
+
+	slotCalculator() {
+        let crew_bonuses = {};
+        if (
+			STTApi.playerData.character.events &&
+			STTApi.playerData.character.events.length > 0 &&
+			STTApi.playerData.character.events[0].content.content_type === 'shuttles' &&
+			STTApi.playerData.character.events[0].opened
+		) {
+			// In a shuttle event
+            let event = STTApi.playerData.character.events[0];
+            crew_bonuses = event.content.shuttles[0].crew_bonuses;
+        }
+
+		let sortedRoster = [];
+		STTApi.roster.forEach(crew => {
+            let bonus = 1;
+            if (crew_bonuses[crew.symbol]) {
+                bonus = crew_bonuses[crew.symbol];
+            }
+
+			sortedRoster.push({
+				crew,
+				command_skill: crew.command_skill_core * bonus,
+				science_skill: crew.science_skill_core * bonus,
+				security_skill: crew.security_skill_core * bonus,
+				engineering_skill: crew.engineering_skill_core * bonus,
+				diplomacy_skill: crew.diplomacy_skill_core * bonus,
+				medicine_skill: crew.medicine_skill_core * bonus,
+            });
+		});
+
+		STTApi.playerData.character.shuttle_adventures.forEach(adventure => {
+			let challenge_rating = adventure.challenge_rating;
+			let shuttle = adventure.shuttles[0];
+
+            // TODO: this assumes there are at most 2 skills in each slot
+            shuttle.calcSlots = [];
+			shuttle.slots.forEach(slot => {
+				let calcSlot = {};
+				if (slot.skills.length === 1) {
+					// AND or single
+                    calcSlot.skills = slot.skills[0].split(',');
+                    if (calcSlot.skills.length === 1) {
+                        calcSlot.type = 'SINGLE';
+                        calcSlot.bestCrew = [...sortedRoster].sort((a, b) => a[calcSlot.skills[0]] - b[calcSlot.skills[0]]);
+                    } else {
+                        calcSlot.type = 'AND';
+                        calcSlot.bestCrew = [...sortedRoster].sort((a, b) => {
+                            let a1 = a[calcSlot.skills[0]];
+                            let a2 = a[calcSlot.skills[1]];
+
+                            let b1 = b[calcSlot.skills[0]];
+                            let b2 = b[calcSlot.skills[1]];
+
+                            let atotal = Math.max(a1, a2) + Math.min(a1, a2) * STTApi.serverConfig.config.shuttle_adventures.secondary_skill_percentage;
+                            let btotal = Math.max(b1, b2) + Math.min(b1, b2) * STTApi.serverConfig.config.shuttle_adventures.secondary_skill_percentage;
+
+                            return atotal - btotal;
+                        });
+                    }
+				} else {
+					// OR
+					calcSlot.type = 'OR';
+                    calcSlot.skills = slot.skills;
+                    calcSlot.bestCrew = [...sortedRoster].sort((a, b) => Math.max(a[calcSlot.skills[0]], a[calcSlot.skills[1]]) - Math.max(b[calcSlot.skills[0]], b[calcSlot.skills[1]]));
+                }
+
+                calcSlot.bestCrew = calcSlot.bestCrew.reverse();
+
+                // TODO: I should cache the presorted lists since more than one slot will share the same config
+
+                console.log(calcSlot);
+                
+                shuttle.calcSlots.push(calcSlot);
+			});
+		});
 	}
 
 	getState(state) {
@@ -88,7 +231,8 @@ export class Shuttles extends React.Component {
 	}
 
 	renderShuttle(shuttle) {
-		let faction = STTApi.playerData.character.factions.find(faction => faction.id === shuttle.faction_id);
+        let faction = STTApi.playerData.character.factions.find(faction => faction.id === shuttle.faction_id);
+        console.log(shuttle);
 
 		return (
 			<Item key={shuttle.id}>
@@ -102,6 +246,7 @@ export class Shuttles extends React.Component {
 						<p>{shuttle.description}</p>
 						<p>Faction: {faction.name}</p>
 						<p>Expires in {formatTimeSeconds(shuttle.expires_in)}</p>
+                        {shuttle.calcSlots.map(calcSlot => <p><b>{calcSlot.skills.join(` ${calcSlot.type} `)}</b> : {calcSlot.bestCrew[0].crew.name}</p>)}
 					</Item.Description>
 					<Item.Extra>
 						State: {this.getState(shuttle.state)}
@@ -137,7 +282,7 @@ export class Shuttles extends React.Component {
 					))}
 				</List>
 				<h4>Next shuttle VP: {event.nextVP}</h4>
-                <h3>Active shuttles</h3>
+				<h3>Active shuttles</h3>
 			</div>
 		);
 	}
